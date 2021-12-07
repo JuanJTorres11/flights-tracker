cbd882a5cbd6869ee747fcb2a6bb78bd0b05022d
Juan José Torres <juanjtorres@pm.me>
2021-12-07 14:56:33 -0500

    Create entry endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("This is the Flights tracker API, to get all the flights use the endpoint /flights")
